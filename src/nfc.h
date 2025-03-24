2735dc7e4abbb8233d25cc450ffd12f45f898a9f
JakesMD <71591438+JakesMD@users.noreply.github.com>
2025-03-24 08:43:08 +0100

    ⚡️ Remove delay

--- src/nfc.h
+++ src/nfc.h
@@ -16,8 +16,6 @@ class YNFC {
     bool begin() { return _nfc.begin(); }
 
     YNFCResult read() {
-        delay(1000);
-
         uint8_t success;
         uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0};  // Buffer to store the returned UID
         uint8_t uidLength;                      // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
